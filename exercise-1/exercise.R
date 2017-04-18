0f26393b73200f06697a8ee76f770c75ad6c2a69
arihan-1560795 <arihan@uw.edu>
2017-04-18 13:50:44 -0700

    Made changes

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -5,24 +5,30 @@ install.packages('devtools')
 
 # Install "fueleconomy" package from GitHub
 devtools::install_github("hadley/fueleconomy")
+install.packages("hadley/fueleconomy")
+read.csv('vehicles.csv')
 
 # Require/library the fueleconomy package
-
+library(fueleconomy)
 
 # You should have have access to the `vehicles` data.frame
-
+data.frame.vehicles.master <- read.csv('vehicles.csv')
+vehicles <- read.csv('vehicles.csv')
 
 # Create a data.frame of vehicles from 1997
-
+data.frame <- data.frame.vehicles.master[data.frame.vehicles.master$year == 1997,]
+#cars.1997[arg1, arg2(what we want)] (Which row, column you want)
+cars.1997 <- vehicles[vehicles$year==1997, ]
 
 # Use the `unique` function to verify that there is only 1 value in the `year` column of your new data.frame
-
+unique(data.frame$year)
+unique(cars.1997$year)
 
 # Create a data.frame of 2-Wheel Drive vehicles that get more than 20 miles/gallon in the city
-
+two.wheel.20.mpg <- vehicles[vehicles$drive == '2-Wheel Drive' & vehicles$cty > 20,]
 
 # Of those vehicles, what is the vehicle ID of the vehicle with the worst hwy mpg?
-
+worst.hwy <- two.wheel.20.mpg$id[two.wheel.20.mpg$hwy == min(two.wheel.20.mpg$hwy)]
 
 # Write a function that takes a `year` and a `make` as parameters, and returns 
 # The vehicle that gets the most hwy miles/gallon of vehicles of that make in that year
--- exercise-2/exercise.R
+++ exercise-2/exercise.R
@@ -10,22 +10,24 @@ devtools::install_github("hadley/fueleconomy")
 # Require/library the fueleconomy package
 
 # Install (if you haven't already) and load the `dplyr`package
-
+install.packages('dplyr')
+library(dplyr)
 
 # You should have have access to the `vehicles` data.frame
-
+vehicles <- read.csv('vehicles.csv')
 
 # Create a data.frame of vehicles from 1997
-
+data.frame <- filter(vehicles, year == 1997)
 
 # Use the `unique` function to verify that there is only 1 value in the `year` column of your new data.frame
-
+unique(data.frame$year)
 
 # Create a data.frame of 2-Wheel Drive vehicles that get more than 20 miles/gallon in the city
-
+wheel.mpg = filter(vehicles, drive == '2-Wheel Drive', cty > 20)
 
 # Of those vehicles, what is the vehicle ID of the vehicle with the worst hwy mpg?
-
+filtered <- filter(wheel.mpg, hwy == min(hwy))
+worst.hwy <- select(filtered, id)
 
 # Write a function that takes a `year` and a `make` as parameters, and returns 
 # The vehicle that gets the most hwy miles/gallon of vehicles of that make in that year
